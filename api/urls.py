4a28c329e09dd214d58e66f6c4f717d524af431d
yashtiwari1906 <yash.tiwari@cars24.com>
2023-11-29 00:51:50 +0530

    first commit

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.authtoken import views
+from .views import home
+
+urlpatterns =[
+    path('', home, name='api.home'),
+    # path('event/',include('api.event.urls')),
+    # path('user/',include('api.user.urls')),
+    # path('booking/',include('api.booking.urls')),
+    
+    # path('user/', include('api.user.urls'))
+]
