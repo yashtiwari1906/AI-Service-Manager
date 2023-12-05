5e72cbcce551cf690a529868c8a6d0a8afa8f7e4
yashtiwari1906 <yash.tiwari@cars24.com>
2023-12-06 01:03:41 +0530

    fix:docker container calls from internal IP of docker network

--- api/chehra/views.py
+++ api/chehra/views.py
@@ -28,8 +28,8 @@ def my_custom_sql(request):
 
 class FaceOperations:
 	def __init__(self) -> None:
-		self.detector_url = "http://127.0.0.1:5001/predict"
-		self.verifier_url = "http://127.0.0.1:4001/predict"
+		self.detector_url = "http://172.18.0.2:5001/predict"
+		self.verifier_url = "http://172.18.0.3:4001/predict"
 	
 	def crop_face_from_frame(self, image, save_image_with_bbox=False):
 		
